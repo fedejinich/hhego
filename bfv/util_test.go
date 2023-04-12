0ab604baa65f7bd2d165d54730c1626d313c008e
fedejinich <jinichfede@gmail.com>
2023-04-12 10:28:41 -0300

    complete matmul implementation for bfv util

--- bfv/util_test.go
+++ bfv/util_test.go
@@ -1,7 +1,16 @@
 package bfv
 
-import "testing"
+import (
+	"github.com/tuneinsight/lattigo/v4/bfv"
+	"github.com/tuneinsight/lattigo/v4/rlwe"
+	"testing"
+)
 
 func TestMatmul(t *testing.T) {
+	params, _ := bfv.NewParametersFromLiteral(bfv.DefaultPostQuantumParams[0])
+	encoder := bfv.NewEncoder(params)
+	evaluationKey := rlwe.EvaluationKey{}
+	evaluator := bfv.NewEvaluator(params, evaluationKey)
 
+	NewUtil(params, encoder, evaluator)
 }

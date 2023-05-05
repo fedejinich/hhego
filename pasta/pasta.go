429ceae59d9d0288919cd1dfa2d271f5e8c91d7a
fedejinich <federico@iovlabs.org>
2023-05-05 16:35:14 -0300

    pasta nonce as constant

--- pasta/pasta.go
+++ pasta/pasta.go
@@ -11,7 +11,8 @@ const CiphertextSize = 128
 const NumMatmulsSquares = 3
 const LastSquare = false
 
-// todo(fedejinich) refactor this to PastaParams
+const Nonce = uint64(123456789)
+
 type Params struct {
 	SecretKeySize  uint64
 	PlaintextSize  uint64
@@ -20,35 +21,34 @@ type Params struct {
 }
 
 type Pasta struct {
-	SecretKey    SecretKey
-	Modulus      uint64
-	CipherParams Params
+	SecretKey SecretKey
+	Modulus   uint64
+	Params    Params
 }
 
-func NewPasta(secretKey []uint64, modulus uint64, cipherParams Params) Pasta {
+func NewPasta(secretKey []uint64, modulus uint64, params Params) Pasta {
 	pasta := Pasta{
 		secretKey,
 		modulus,
-		cipherParams,
+		params,
 	}
 
 	return pasta
 }
 
 func (p *Pasta) Encrypt(plaintext []uint64) []uint64 {
-	nonce := uint64(123456789)
 	size := len(plaintext)
 
-	numBlock := int(math.Ceil(float64(size) / float64(p.CipherParams.PlaintextSize)))
+	numBlock := int(math.Ceil(float64(size) / float64(p.Params.PlaintextSize)))
 
-	pastaUtil := NewUtil(p.SecretKey, p.Modulus, int(p.CipherParams.Rounds))
+	pastaUtil := NewUtil(p.SecretKey, p.Modulus, int(p.Params.Rounds))
 	ciphertext := make([]uint64, size)
 	copy(ciphertext, plaintext)
 
 	for b := uint64(0); b < uint64(numBlock); b++ {
-		ks := pastaUtil.Keystream(nonce, b)
-		for i := int(b * p.CipherParams.PlaintextSize); i < int((b+1)*p.CipherParams.PlaintextSize) && i < size; i++ {
-			ciphertext[i] = (ciphertext[i] + ks[i-int(b*p.CipherParams.PlaintextSize)]) % p.Modulus
+		ks := pastaUtil.Keystream(Nonce, b)
+		for i := int(b * p.Params.PlaintextSize); i < int((b+1)*p.Params.PlaintextSize) && i < size; i++ {
+			ciphertext[i] = (ciphertext[i] + ks[i-int(b*p.Params.PlaintextSize)]) % p.Modulus
 		}
 	}
 
@@ -56,22 +56,21 @@ func (p *Pasta) Encrypt(plaintext []uint64) []uint64 {
 }
 
 func (p *Pasta) Decrypt(ciphertext []uint64) []uint64 {
-	nonce := uint64(123456789)
 	size := len(ciphertext)
 
-	numBlock := int(math.Ceil(float64(size) / float64(p.CipherParams.CiphertextSize)))
+	numBlock := int(math.Ceil(float64(size) / float64(p.Params.CiphertextSize)))
 
-	pasta := NewUtil(p.SecretKey, p.Modulus, int(p.CipherParams.Rounds))
+	pasta := NewUtil(p.SecretKey, p.Modulus, int(p.Params.Rounds))
 	plaintext := make([]uint64, size)
 	copy(plaintext, ciphertext)
 
 	for b := uint64(0); b < uint64(numBlock); b++ {
-		ks := pasta.Keystream(nonce, b)
-		for i := int(b * p.CipherParams.CiphertextSize); i < int((b+1)*p.CipherParams.CiphertextSize) && i < size; i++ {
-			if ks[i-int(b*p.CipherParams.PlaintextSize)] > plaintext[i] {
+		ks := pasta.Keystream(Nonce, b)
+		for i := int(b * p.Params.CiphertextSize); i < int((b+1)*p.Params.CiphertextSize) && i < size; i++ {
+			if ks[i-int(b*p.Params.PlaintextSize)] > plaintext[i] {
 				plaintext[i] += p.Modulus
 			}
-			plaintext[i] = plaintext[i] - ks[i-int(b*p.CipherParams.PlaintextSize)]
+			plaintext[i] = plaintext[i] - ks[i-int(b*p.Params.PlaintextSize)]
 		}
 	}
 
